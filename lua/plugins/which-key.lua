3af6bfae6bb8d9592b9cfbd0aea9c18949b97072
DuarteAd <adrien.duarte65@laposte.net>
2025-09-23 00:17:31 +0200

    chore : refacto

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,46 +2,10 @@ return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
   event = 'VeryLazy', -- Sets the loading event to 'VimEnter'
   opts = {
+    preset = 'helix', -- classic, helix, modern
     -- delay between pressing a key and opening which-key (milliseconds)
     -- this setting is independent of vim.o.timeoutlen
     delay = 0,
-    icons = {
-      -- set icon mappings to true if you have a Nerd Font
-      mappings = vim.g.have_nerd_font,
-      -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
-      -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
-      keys = vim.g.have_nerd_font and {} or {
-        Up = '<Up> ',
-        Down = '<Down> ',
-        Left = '<Left> ',
-        Right = '<Right> ',
-        C = '<C-…> ',
-        M = '<M-…> ',
-        D = '<D-…> ',
-        S = '<S-…> ',
-        CR = '<CR> ',
-        Esc = '<Esc> ',
-        ScrollWheelDown = '<ScrollWheelDown> ',
-        ScrollWheelUp = '<ScrollWheelUp> ',
-        NL = '<NL> ',
-        BS = '<BS> ',
-        Space = '<Space> ',
-        Tab = '<Tab> ',
-        F1 = '<F1>',
-        F2 = '<F2>',
-        F3 = '<F3>',
-        F4 = '<F4>',
-        F5 = '<F5>',
-        F6 = '<F6>',
-        F7 = '<F7>',
-        F8 = '<F8>',
-        F9 = '<F9>',
-        F10 = '<F10>',
-        F11 = '<F11>',
-        F12 = '<F12>',
-      },
-    },
-
     -- Document existing key chains
     spec = {
       { '<leader>s', group = 'Search' },
@@ -49,6 +13,13 @@ return { -- Useful plugin to show you pending keybinds.
       { '<leader>g', group = 'Git', mode = { 'n', 'v' } },
       { '<leader>b', group = 'Buffers', icon = { icon = ' ', color = 'magenta' } },
       { '<leader>bs', group = 'Sort', icon = { icon = '󰒺 ' } },
+    },
+    win = {
+      no_overlap = true, -- Prevent the WhichKey window from overlapping with the cursor
+    },
+    keys = {
+      scroll_down = '<C-d>', -- binding to scroll down inside the which-key popup
+      scroll_up = '<C-u>', -- binding to scroll up inside the which-key popup
     },
   },
 }
