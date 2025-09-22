15f20b3b27d3ae020ce813d8a061fb3be3614103
DuarteAd <adrien.duarte65@laposte.net>
2025-09-22 04:32:54 +0200

    fix : rm warning latex

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -5,5 +5,8 @@ return {
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
-  opts = {},
+  opts = {
+    -- to disable the health warning, install latex support if i want to use it
+    latex = { enabled = false },
+  },
 }
